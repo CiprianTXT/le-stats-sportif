7164988572cc9d2e14f5d50796f1deac9cfbdaa7
CiprianTXT <ciprian.popescu0411@stud.acs.upb.ro>
2024-04-10 19:35:09 +0300

    Defined "states_mean" request

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ def num_jobs_request():
 def jobs_request():
     jobs = []
     for current_id in range(1, webserver.job_counter):
-        if os.path.exists(f"./results/job_id_{current_id}"):
+        if webserver.tasks_runner.job_status[current_id] == "done":
             jobs.append({f"job_id_{current_id}": "done"})
         else:
             jobs.append({f"job_id_{current_id}": "running"})
@@ -25,39 +25,50 @@ def jobs_request():
 
 @webserver.route('/api/get_results/<job_id>', methods=['GET'])
 def get_results_request(job_id):
+    job_id = int(job_id)
     print(f"JobID is {job_id}")
 
     # Check if job_id is valid
-    if not int(job_id) in range(1, webserver.job_counter):
+    if not job_id in range(1, webserver.job_counter):
         return jsonify({
             "status": "error",
             "reason": "Invalid job_id"
         })
 
     # Check if job_id is done and return the result
-    try:
+    if webserver.tasks_runner.job_status[job_id] == "done":
         with open(f"./results/job_id_{job_id}.json") as file:
             result = json.load(file)
         return jsonify({
             "status": "done",
             "data": result
         })
+
     # If not, return running status
-    except FileNotFoundError:
-        return jsonify({"status": "running"})
+    return jsonify({"status": "running"})
 
 @webserver.route('/api/states_mean', methods=['POST'])
 def states_mean_request():
-    # Get request data
-    data = request.json
-    print(f"Got request {data}")
+    if webserver.tasks_runner.is_running():
+        # Get request data
+        data = request.json
+        print(f"Got request {data}")
 
-    # TODO
-    # Register job. Don't wait for task to finish
-    # Increment job_id counter
-    # Return associated job_id
+        # Register job. Don't wait for task to finish
+        job_id = webserver.job_counter
+        job = ["states_mean", data["question"], job_id]
+        webserver.tasks_runner.job_queue.put(job)
 
-    return jsonify({"status": "NotImplemented"})
+        # Increment job_id counter
+        webserver.job_counter += 1
+
+        # Return associated job_id
+        return jsonify({
+            "status": "queued",
+            "job_id": job_id
+        })
+    else:
+        return jsonify({"status": "Shutting down"})
 
 @webserver.route('/api/state_mean', methods=['POST'])
 def state_mean_request():
