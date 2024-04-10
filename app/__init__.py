e4b94cfeb94c70a9ec7b28a205dff4765f15b055
CiprianTXT <ciprian.popescu0411@stud.acs.upb.ro>
2024-04-10 19:33:38 +0300

    Added initial checks from "task_runner.py"

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
+import os
+
+# Creating the results folder if not present
+if not os.path.exists("./results"):
+    os.mkdir("./results")
+
+# Checking the number of threads to create
+if 'TP_NUM_OF_THREADS' in os.environ:
+    num_of_threads = os.environ.get("TP_NUM_OF_THREADS")
+else:
+    num_of_threads = os.cpu_count()
 
 webserver = Flask(__name__)
-webserver.tasks_runner = ThreadPool()
-
-# webserver.task_runner.start()
-
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
+
+webserver.tasks_runner = ThreadPool(num_of_threads, webserver.data_ingestor.table)
 
 webserver.job_counter = 1
 
